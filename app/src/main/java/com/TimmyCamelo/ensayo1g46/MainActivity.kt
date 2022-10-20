1e29dd9c69ca78ddae6531b8bc94477602c1e5fa
MonicaCamelo <monicacamelocastillo@gmail.com>
2022-10-20 10:39:58 -0500

    primer commit de proyecto desarrollo movil

---
+++ app/src/main/java/com/TimmyCamelo/ensayo1g46/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.TimmyCamelo.ensayo1g46
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
